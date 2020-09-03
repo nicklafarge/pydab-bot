da5d554c415dfd5b64b3a9317080b184667c57ec
Nick LaFarge <nick.lafarge@gmail.com>
2020-09-03 14:38:04 -0400

    bean restore

--- app/dabber.py
+++ app/dabber.py
@@ -34,6 +34,7 @@ message_triggers = [
     MessageTrigger(['charlie', 'wagon'], 'chuckwagon', trigger_type=TriggerType.REACTION),
     MessageTrigger(['roxy', 'roxstar'], 'roxstar', trigger_type=TriggerType.REACTION),
     MessageTrigger(['sherpa'], 'sherpa', trigger_type=TriggerType.REACTION),
+    MessageTrigger(['bean', 'coffee', 'latte', '3bs'], 'thosbeans', trigger_type=TriggerType.REACTION),
     MessageTrigger(['lucy'], 'lucy', trigger_type=TriggerType.REACTION),
     MessageTrigger(['mischief'], 'lucyintheskywithdiamonds', trigger_type=TriggerType.REACTION),
 
